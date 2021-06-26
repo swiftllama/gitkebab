c2fa4c034437d061f6ba1d2a5412bc33fd41ab69
volund <volund@gmail.com>
2021-06-26 15:07:55 +0200

    fixed crash

--- src/gk_credentials.c
+++ src/gk_credentials.c
@@ -16,9 +16,9 @@ gk_result_t *gk_session_credential_ssh_key_memory_init(gk_session_credential_t *
     }
 
     credential->credential_type = CREDENTIAL_SSH_KEY_MEMORY;
-    credential->ssh_private_key_bytes = strdup(private_key_bytes);
-    credential->ssh_public_key_bytes = strdup(public_key_bytes);
-    credential->ssh_private_key_passphrase = strdup(private_key_passphrase);
+    credential->ssh_private_key_bytes = private_key_bytes == NULL ? NULL : strdup(private_key_bytes);
+    credential->ssh_public_key_bytes = public_key_bytes == NULL ? NULL : strdup(public_key_bytes);
+    credential->ssh_private_key_passphrase = private_key_passphrase == NULL ? NULL : strdup(private_key_passphrase);
 
     return gk_result_success();
 }
@@ -33,9 +33,9 @@ gk_result_t *gk_session_credential_ssh_key_file_init(gk_session_credential_t *cr
     }
 
     credential->credential_type = CREDENTIAL_SSH_KEY_FILE;
-    credential->ssh_private_key_path = strdup(private_key_path);
-    credential->ssh_public_key_path = strdup(public_key_path);
-    credential->ssh_private_key_passphrase = strdup(private_key_passphrase);
+    credential->ssh_private_key_path = private_key_path == NULL ? NULL : strdup(private_key_path);
+    credential->ssh_public_key_path = public_key_path == NULL ? NULL : strdup(public_key_path);
+    credential->ssh_private_key_passphrase = private_key_passphrase == NULL ? NULL : strdup(private_key_passphrase);
     
     return gk_result_success();
 }
@@ -50,8 +50,8 @@ gk_result_t *gk_session_credential_username_password_init(gk_session_credential_
     }
 
     credential->credential_type = CREDENTIAL_USERNAME_PASSWORD;
-    credential->username = strdup(username);
-    credential->password = strdup(password);
+    credential->username = username == NULL ? NULL : strdup(username);
+    credential->password = password == NULL ? NULL : strdup(password);
 
     return gk_result_success();
 }
