0f165885c42cff24cfa930efb1e1a7e852a765bc
volund <volund@gmail.com>
2021-07-01 22:25:25 +0200

    initial status

--- src/lib/gk_status.c
+++ src/lib/gk_status.c
@@ -1,24 +1,19 @@
 
 #include "gk_status.h"
+#include "git2.h"
 
+void gk_repository_status_summary(gk_repository_t *repo, gk_repository_status_summary_t *status_summary) {
+    git_status_options status_options = GIT_STATUS_OPTIONS_INIT;
+    
+    status_options.show  = GIT_STATUS_SHOW_INDEX_AND_WORKDIR;
+    status_options.flags = GIT_STATUS_OPT_INCLUDE_UNTRACKED |
+        GIT_STATUS_OPT_RECURSE_UNTRACKED_DIRS |
+        GIT_STATUS_OPT_RENAMES_INDEX_TO_WORKDIR | 
+        GIT_STATUS_OPT_RENAMES_HEAD_TO_INDEX |
+        GIT_STATUS_OPT_SORT_CASE_INSENSITIVELY;
 
-git_status_t
+    
+}
 
-Status flags for a single file.
+void gk_repository_status_summary_free_members(gk_repository_status_summary_t *status_summary);
 
-A combination of these values will be returned to indicate the status of a file. Status compares the working directory, the index, and the current HEAD of the repository. The GIT_STATUS_INDEX set of flags represents the status of file in the index relative to the HEAD, and the GIT_STATUS_WT set of flags represent the status of the file in the working directory relative to the index.
-
-GIT_STATUS_CURRENT
-GIT_STATUS_INDEX_NEW
-GIT_STATUS_INDEX_MODIFIED
-GIT_STATUS_INDEX_DELETED
-GIT_STATUS_INDEX_RENAMED
-GIT_STATUS_INDEX_TYPECHANGE
-GIT_STATUS_WT_NEW
-GIT_STATUS_WT_MODIFIED
-GIT_STATUS_WT_DELETED
-GIT_STATUS_WT_TYPECHANGE
-GIT_STATUS_WT_RENAMED
-GIT_STATUS_WT_UNREADABLE
-GIT_STATUS_IGNORED
-GIT_STATUS_CONFLICTED
--- src/lib/gk_status.h
+++ src/lib/gk_status.h
@@ -2,9 +2,17 @@
 #ifndef __GK_STATUS_H__
 #define __GK_STATUS_H__
 
-void gk_repository_status_summary(..);
+#include "gk_session.h"
+
 
 typedef struct {
+    unsigned long count_new;
+    unsigned long count_modified;
+    unsigned long count_deleted;
+    unsigned long count_renamed;
+    unsigned long count_typechanged;
+    unsigned long count_conflicted;
+    unsigned long count_total;
     const char *new[];
     const char *modified[];
     const char *deleted[];
@@ -12,4 +20,9 @@ typedef struct {
     const char *typechange[];
     const char *conflicted[];
 } gk_repository_status_summary_t;
+
+void gk_repository_status_summary(gk_repository_t *repo, gk_repository_status_summary_t *status_summary);
+
+void gk_repository_status_summary_free_members(gk_repository_status_summary_t *status_summary);
+
 #endif // __GK_STATUS_H__
