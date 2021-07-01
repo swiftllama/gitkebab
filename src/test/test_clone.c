ed16baf4695d1ed7d1602fcc1ac1d1137b31bae4
volund <volund@gmail.com>
2021-07-01 12:09:06 +0200

    refactored

--- src/test/test_clone.c
+++ src/test/test_clone.c
@@ -6,9 +6,9 @@
 #include "gitkebab.h"
 #include "gk_test_filesystem_utils.h"
 
-void session_progress(gk_session_progress_t *progress) {
-    //printf("[SESSION PROGRESS] %s (TOTAL: %d%%)\n", progress->description, progress->percent);
-}
+void session_progress(gk_session_progress_t *progress) { }
+
+gk_session_credential_t g_empty_credential;
 
 static int test_staging_setup(void **state) {
     gk_init();
@@ -17,21 +17,24 @@ static int test_staging_setup(void **state) {
         log_warn(COMP_TEST, "test staging directory 'test-staging' found to exist during test setup, this could mean a previous test was aborted mid-way. Removing");
         rm_rf("test-staging");
     }
-    else {
-        log_warn(COMP_TEST, "test-staging directory does not exist");
-    }
         
     create_directory("test-staging");
     create_directory("test-staging/empty-dir1");
     create_directory("test-staging/nonempty-dir1");
     create_directory("test-staging/nonempty-dir1/child");
-        
+    
+    gk_session_credential_username_password_init(&g_empty_credential, "", "");
+
+    return 0;
 }
 
 static int test_staging_teardown(void **state) {
     (void) state; /* unused */
-    log_info(COMP_TEST, "Group teardown");
-    //rm_rf("test-staging");
+    gk_session_credential_free_members(&g_empty_credential);
+
+    rm_rf("test-staging");
+
+    return 0;
 }
 
 
@@ -42,11 +45,7 @@ static void test_clone_simple(void **state) {
     gk_repository_init(&repo, "./src/test/fixtures/simple-repo1.git/", "./test-staging/clone-test-1", "git");
     gk_session_t session;
     gk_session_init(&session, &repo, &session_progress);
-    gk_session_credential_t credential;
-    gk_session_credential_username_password_init(&credential, "", "");
-    gk_session_clone(&session, &credential);
-
-    gk_session_credential_free_members(&credential);
+    gk_session_clone(&session, &g_empty_credential);
 
     assert_int_equal(file_exists("test-staging/clone-test-1/file1"), 0);
     assert_int_equal(file_exists("test-staging/clone-test-1/file2"), 0);
@@ -61,11 +60,7 @@ static void test_clone_bad_source_path(void **state) {
     gk_repository_init(&repo, "test-staging/tmp/non-existent-path/", "./test-staging/clone-test-2", "git");
     gk_session_t session;
     gk_session_init(&session, &repo, &session_progress);
-    gk_session_credential_t credential;
-    gk_session_credential_username_password_init(&credential, "", "");
-    gk_session_clone(&session, &credential);
-
-    gk_session_credential_free_members(&credential);
+    gk_session_clone(&session, &g_empty_credential);
 
     assert_int_not_equal(gk_result_code(session.last_result), 0);
 }
@@ -77,11 +72,7 @@ static void test_clone_null_dest_path(void **state) {
     gk_repository_init(&repo, "src/test/fixtures/simple-repo1.git/", NULL, "git");
     gk_session_t session;
     gk_session_init(&session, &repo, &session_progress);
-    gk_session_credential_t credential;
-    gk_session_credential_username_password_init(&credential, "", "");
-    gk_session_clone(&session, &credential);
-
-    gk_session_credential_free_members(&credential);
+    gk_session_clone(&session, &g_empty_credential);
 
     assert_int_not_equal(gk_result_code(session.last_result), 0);
 }
@@ -93,11 +84,7 @@ static void test_clone_dest_path_empty_existing_regular_dir(void **state) {
     gk_repository_init(&repo, "src/test/fixtures/simple-repo1.git/", "test-staging/empty-dir1", "git");
     gk_session_t session;
     gk_session_init(&session, &repo, &session_progress);
-    gk_session_credential_t credential;
-    gk_session_credential_username_password_init(&credential, "", "");
-    gk_session_clone(&session, &credential);
-
-    gk_session_credential_free_members(&credential);
+    gk_session_clone(&session, &g_empty_credential);
 
     assert_int_equal(gk_result_code(session.last_result), 0);
 }
@@ -109,11 +96,7 @@ static void test_clone_dest_path_nonempty_existing_regular_dir(void **state) {
     gk_repository_init(&repo, "src/test/fixtures/simple-repo1.git/", "test-staging/nonempty-dir1", "git");
     gk_session_t session;
     gk_session_init(&session, &repo, &session_progress);
-    gk_session_credential_t credential;
-    gk_session_credential_username_password_init(&credential, "", "");
-    gk_session_clone(&session, &credential);
-
-    gk_session_credential_free_members(&credential);
+    gk_session_clone(&session, &g_empty_credential);
 
     assert_int_not_equal(gk_result_code(session.last_result), 0);
 }
