9aca74f1377eec258bc80eeb96bb102213a1e5ba
volund <volund@gmail.com>
2021-08-24 16:21:07 +0200

    openssl bashification

---
+++ build-workspace/scripts/openssl-1.1.1k/build/android/arm64-debug/run-direct.sh
@@ -0,0 +1,29 @@
+set -e
+source "scripts/common/bash/common.sh"
+set -x
+
+define_basic_variables "openssl-1.1.1k" "android" "arm64" "debug"
+init_and_change_into_tmp_build_folder
+
+## NOTES
+##  - allow linking resulting static library against shared library later on
+##    - "-fPIC"
+##  - openssl requires ANDROID_NDK_ROOT to be defined and PATH to be exported
+##    To a location containing the pre-built android toolchain binaries
+
+define_android_variables
+
+export PATH=$NDK_TOOLCHAIN/bin/:$PATH/
+export ANDROID_NDK_HOME=$NDK
+${RELATIVE_SOURCE}/Configure \
+                  android-arm64 \
+                  -D__ANDROID_API__=$API \
+                  --prefix=${ROOT}/${BUILD_FOLDER} \
+                  --openssldir=${ROOT}/${BUILD_FOLDER} \
+                  -fPIC 
+
+make
+android_objdump_verify_library_architecture "${ROOT}/${TMP_BUILD_FOLDER}/libssl.a" "aarch64"
+make install
+
+print_done
