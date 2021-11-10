31d1c1126bd04e0a309b265aa08d617a0a4bec62
amos <amos@amoss-iMac.local>
2021-11-10 10:51:36 +0100

    libgit2 macos patch

--- build-workspace/scripts/libgit2-1.1.1/source/clone.sh
+++ build-workspace/scripts/libgit2-1.1.1/source/clone.sh
@@ -6,9 +6,10 @@ rm -rf source/libgit2-1.1.1
 mkdir -p source
 git clone --branch v1.1.1 --depth 1 git@github.com:libgit2/libgit2.git source/libgit2-1.1.1
 
-echo "Applying patch"
+echo "Applying patches"
 cd source/libgit2-1.1.1
 patch -p0 < ../../patches/libgit2-1.1.1/fix-userauth-publikey-frommemory-detection.diff
+git apply ../../patches/libgit2-1.1.1/fix-macos-eintr-handling.diff
 
 set +x
 echo "--- DONE ---"
