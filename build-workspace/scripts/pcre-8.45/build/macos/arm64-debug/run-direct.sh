1232d43694d3a5f3afaf99dcf04cc4b1d88be48a
Amos <volund@gmail.com>
2022-05-27 07:18:40 +0200

    fix mixup

--- build-workspace/scripts/pcre-8.45/build/macos/arm64-debug/run-direct.sh
+++ build-workspace/scripts/pcre-8.45/build/macos/arm64-debug/run-direct.sh
@@ -10,8 +10,7 @@ init_and_change_into_tmp_build_folder
 ##    - CFLAGS=-fPIC
 ##
 
-FLAGS="-fPIC --target=x86_64-apple-macos -arch x86_64 -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX12.3.sdk -mmacosx-version-min=10.11"
-${RELATIVE_SOURCE}/configure --prefix=${ROOT}/${BUILD_FOLDER} CFLAGS="${FLAGS}" CXXFLAGS="${FLAGS}" CC="/Library/Developer/CommandLineTools/usr/bin/cc" LDFLAGS="${FLAGS}"
+${RELATIVE_SOURCE}/configure --prefix=${ROOT}/${BUILD_FOLDER} CFLAGS="-fPIC --target=x86_64-apple-macos11" CXXFLAGS="-fPIC -mmacosx-version-min=10.11"
 make
 make install
 
--- build-workspace/scripts/pcre-8.45/build/macos/x86_64-debug/run-direct.sh
+++ build-workspace/scripts/pcre-8.45/build/macos/x86_64-debug/run-direct.sh
@@ -10,7 +10,8 @@ init_and_change_into_tmp_build_folder
 ##    - CFLAGS=-fPIC
 ##
 
-${RELATIVE_SOURCE}/configure --prefix=${ROOT}/${BUILD_FOLDER} CFLAGS="-fPIC --target=x86_64-apple-macos11" CXXFLAGS="-fPIC -mmacosx-version-min=10.11"
+FLAGS="-fPIC --target=x86_64-apple-macos -arch x86_64 -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX12.3.sdk -mmacosx-version-min=10.11"
+${RELATIVE_SOURCE}/configure --prefix=${ROOT}/${BUILD_FOLDER} CFLAGS="${FLAGS}" CXXFLAGS="${FLAGS}" CC="/Library/Developer/CommandLineTools/usr/bin/cc" LDFLAGS="${FLAGS}"
 make
 make install
 
