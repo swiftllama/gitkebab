32464fc52d7db39d1b0eba4b01856e00e3d22f59
Amos <volund@gmail.com>
2022-05-27 08:45:39 +0200

    update gitkebab libiconv dependency

--- build-workspace/scripts/libiconv-1.17/build/macos/x86_64-debug/run-direct.sh
+++ build-workspace/scripts/libiconv-1.17/build/macos/x86_64-debug/run-direct.sh
@@ -11,7 +11,7 @@ init_and_change_into_tmp_build_folder
 ##
 
 FLAGS="-fPIC --target=x86_64-apple-macos -arch x86_64 -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX12.3.sdk -mmacosx-version-min=10.11"
-${RELATIVE_SOURCE}/configure --enable-static --prefix=${ROOT}/${BUILD_FOLDER} CFLAGS="${FLAGS}" CXXFLAGS="${FLAGS}" CC="/Library/Developer/CommandLineTools/usr/bin/cc" LDFLAGS="${FLAGS}"
+${RELATIVE_SOURCE}/configure --enable-static --prefix=${ROOT}/${BUILD_FOLDER} CFLAGS="${FLAGS}" CXXFLAGS="${FLAGS}" LDFLAGS="${FLAGS}"
 make
 make install
 
