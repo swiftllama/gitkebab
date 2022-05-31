12a8fb4901d082fdd3301e98a87bd9bc6af6dc99
Amos <volund@gmail.com>
2022-05-31 05:33:43 +0200

    pcre iphone simulator

--- build-workspace/scripts/pcre-8.45/build/ios/universal-debug/run-direct-simulator.sh
+++ build-workspace/scripts/pcre-8.45/build/ios/universal-debug/run-direct-simulator.sh
@@ -8,15 +8,15 @@ init_and_change_into_tmp_build_folder
 XCODE_ROOT=/Applications/Xcode.app
 IOS_SDK_VERSION=15.5
 IOS_TARGET_VERSION=10.0
-IOS_SDK=${XCODE_ROOT}/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS${IOS_SDK_VERSION}.sdk
-ARCHS="armv7;armv7s;arm64"
+IOS_SDK=${XCODE_ROOT}/Contents/Developer/Platforms/iPhoneSimlator.platform/Developer/SDKs/iPhoneSimulator${IOS_SDK_VERSION}.sdk
+ARCHS="i386;x86_64"
 
 cmake -DBUILD_SHARED_LIBS=NO \
       -DCMAKE_C_COMPILER_WORKS=ON \
       -DCMAKE_CXX_COMPILER_WORKS=ON \
       -DWITH_STATIC_LIB=true \
       -DCMAKE_BUILD_TYPE=Debug \
-      -DCMAKE_INSTALL_PREFIX=${ROOT}/${BUILD_FOLDER}_device  \
+      -DCMAKE_INSTALL_PREFIX=${ROOT}/${BUILD_FOLDER}_simulator  \
       -DPCRE_BUILD_PCRECPP=NO \
       -DPCRE_BUILD_PCREGREP=NO \
       -DPCRE_BUILD_TESTS=NO \
