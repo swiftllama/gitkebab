1bd171263e950e0d9546894ac1854bab91e48351
Amos <volund@gmail.com>
2022-05-31 04:18:50 +0200

    ios build script

--- build-workspace/scripts/zlib-1.2.12/build/ios/universal-debug/run-direct.sh
+++ build-workspace/scripts/zlib-1.2.12/build/ios/universal-debug/run-direct.sh
@@ -5,18 +5,13 @@ set -x
 define_basic_variables "zlib-1.2.12" "ios" "universal" "debug"
 init_and_change_into_tmp_build_folder
 
-## NOTES
-##  - allow linking resulting static library against shared library later on
-##    - CFLAGS=-fPIC
-##
-
 XCODE_ROOT=/Applications/Xcode.app
 IOS_SDK_VERSION=15.5
 IOS_TARGET_VERSION=10.0
 IOS_SDK=${XCODE_ROOT}/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDK/iPhoneOS${IOS_SDK_VERSION}.sdk
 
 
-CFLAGS="-O3 -arch i386 -arch x86_64 -isysroot $IOS_SDK -mios-simulator-version-min=${IOS_TARGET_VERSION} -mios-simulator-version-min=${IOS_TARGET_VERSION} -Wno-error-implicit-function-declaration -fembed-bitcode"
+CFLAGS="-O3 -arch i386 -arch x86_64 -isysroot $IOS_SDK -mios-simulator-version-min=${IOS_TARGET_VERSION} -mios-simulator-version-min=${IOS_TARGET_VERSION} -Wno-error-implicit-function-declaration -fembed-bitcode" ${RELATIVE_SOURCE}/configure --prefix=${ROOT}/${BUILD_FOLDER}
 make
 make install
 
