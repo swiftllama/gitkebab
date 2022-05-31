35a7a2cff9c57f9791fc5df35f37d82180419936
Amos <volund@gmail.com>
2022-05-31 05:39:38 +0200

    pcre ios universal

--- build-workspace/scripts/pcre-8.45/build/ios/universal-debug/check.sh
+++ build-workspace/scripts/pcre-8.45/build/ios/universal-debug/check.sh
@@ -7,3 +7,19 @@ define_build_folders
 if ! check_sentinel_exists "${BUILD_FOLDER}/lib/libpcre.a"; then
     exit 1;
 fi
+
+if ! macos_lipo_verify_library_architecture "${BUILD_FOLDER}/lib/libpcre.a" "i386"; then
+    exit 1
+fi
+if ! macos_lipo_verify_library_architecture "${BUILD_FOLDER}/lib/libpcre.a" "x86_64"; then
+    exit 1
+fi
+if ! macos_lipo_verify_library_architecture "${BUILD_FOLDER}/lib/libpcre.a" "armv7"; then
+    exit 1
+fi
+if ! macos_lipo_verify_library_architecture "${BUILD_FOLDER}/lib/libpcre.a" "armv7s"; then
+    exit 1
+fi
+if ! macos_lipo_verify_library_architecture "${BUILD_FOLDER}/lib/libpcre.a" "arm64"; then
+    exit 1
+fi
---
+++ build-workspace/scripts/pcre-8.45/build/ios/universal-debug/run-direct-universal.sh
@@ -0,0 +1,13 @@
+set -e
+source "scripts/common/bash/common.sh"
+set -x
+
+define_basic_variables "pcre-8.45" "ios" "universal" "debug"
+init_and_change_into_tmp_build_folder
+
+mkdir -p ${ROOT}/${BUILD_FOLDER}/lib
+lipo -create ${ROOT}/${BUILD_FOLDER}_device/lib/libpcre.a ${ROOT}/${BUILD_FOLDER}_simulator/lib/libpcre.a -output ${ROOT}/${BUILD_FOLDER}/lib/libpcre.a
+lipo -create ${ROOT}/${BUILD_FOLDER}_device/lib/libpcrecpp.a ${ROOT}/${BUILD_FOLDER}_simulator/lib/libpcrecpp.a -output ${ROOT}/${BUILD_FOLDER}/lib/libpcrecpp.a
+lipo -create ${ROOT}/${BUILD_FOLDER}_device/lib/libpcreposix.a ${ROOT}/${BUILD_FOLDER}_simulator/lib/libpcreposix.a -output ${ROOT}/${BUILD_FOLDER}/lib/libpcreposix.a
+
+print_done
