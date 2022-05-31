1d26ddf5e04ea9cbbe72b8fbc7f481b198e71482
Amos <volund@gmail.com>
2022-05-31 07:09:22 +0200

    openssl ios

--- build-workspace/scripts/openssl-1.1.1n/build/ios/universal-debug/run-direct_universal.sh
+++ build-workspace/scripts/openssl-1.1.1n/build/ios/universal-debug/run-direct_universal.sh
@@ -5,7 +5,7 @@ set -x
 define_basic_variables "openssl-1.1.1n" "ios" "universal" "debug"
 init_and_change_into_tmp_build_folder
 
-lipo -create ${ROOT}/${BUILD_FOLDER}_device-armv7/lib/libssl.a ${ROOT}/${BUILD_FOLDER}_device-armv64/lib/libssl.a ${ROOT}/${BUILD_FOLDER}/lib/libssl.a.simulator  -output ${ROOT}/${BUILD_FOLDER}/lib/libssl.a
-lipo -create ${ROOT}/${BUILD_FOLDER}_device-armv7/lib/libcrypto.a ${ROOT}/${BUILD_FOLDER}_device-armv64/lib/libcrypto.a ${ROOT}/${BUILD_FOLDER}/lib/libcrypto.a.simulator  -output ${ROOT}/${BUILD_FOLDER}/lib/libcrypto.a
+lipo -create ${ROOT}/${BUILD_FOLDER}_device-armv7/lib/libssl.a ${ROOT}/${BUILD_FOLDER}_device-arm64/lib/libssl.a ${ROOT}/${BUILD_FOLDER}/lib/libssl.a.simulator  -output ${ROOT}/${BUILD_FOLDER}/lib/libssl.a
+lipo -create ${ROOT}/${BUILD_FOLDER}_device-armv7/lib/libcrypto.a ${ROOT}/${BUILD_FOLDER}_device-arm64/lib/libcrypto.a ${ROOT}/${BUILD_FOLDER}/lib/libcrypto.a.simulator  -output ${ROOT}/${BUILD_FOLDER}/lib/libcrypto.a
 
 print_done
