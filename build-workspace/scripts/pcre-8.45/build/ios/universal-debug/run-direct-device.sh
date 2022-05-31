b2f76a59512f3be879aa846ef4f9067dd229ebad
Amos <volund@gmail.com>
2022-05-31 05:29:44 +0200

    pcre ios universal

--- build-workspace/scripts/pcre-8.45/build/ios/universal-debug/run-direct-device.sh
+++ build-workspace/scripts/pcre-8.45/build/ios/universal-debug/run-direct-device.sh
@@ -5,20 +5,27 @@ set -x
 define_basic_variables "pcre-8.45" "ios" "universal" "debug"
 init_and_change_into_tmp_build_folder
 
-## NOTES
-##  - allow linking resulting static library against shared library later on
-##    - CFLAGS=-fPIC
-##
-
 XCODE_ROOT=/Applications/Xcode.app
 IOS_SDK_VERSION=15.5
 IOS_TARGET_VERSION=10.0
 IOS_SDK=${XCODE_ROOT}/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS${IOS_SDK_VERSION}.sdk
+ARCHS=armv7;armv7s;arm64
 
+cmake -DBUILD_SHARED_LIBS=NO \
+      -DCMAKE_C_COMPILER_WORKS=ON \
+      -DCMAKE_CXX_COMPILER_WORKS=ON \
+      -DWITH_STATIC_LIB=true \
+      -DCMAKE_BUILD_TYPE=Debug \
+      -DCMAKE_INSTALL_PREFIX=${ROOT}/${BUILD_FOLDER}  \
+      -DPCRE_BUILD_PCRECPP=NO \
+      -DPCRE_BUILD_PCREGREP=NO \
+      -DPCRE_BUILD_TESTS=NO \
+      -DPCRE_SUPPORT_LIBBZ2=NO) \
+      -DCMAKE_OSX_ARCHITECTURES=$ARCHS \
+      -DCMAKE_OSX_SYSROOT=$IOS_SDK \
+      ${RELATIVE_SOURCE} 
 
-FLAGS="-O3 -arch armv7 -arch armv7s -arch arm64 -isysroot $IOS_SDK -mios-version-min=${IOS_TARGET_VERSION} -fembed-bitcode"
-${RELATIVE_SOURCE}/configure --prefix=${ROOT}/${BUILD_FOLDER}_device CFLAGS="$FLAGS" CXXFLAGS="$FLAGS"
-make
-make install
+cmake --build .
+cmake --build . --target install
 
 print_done
