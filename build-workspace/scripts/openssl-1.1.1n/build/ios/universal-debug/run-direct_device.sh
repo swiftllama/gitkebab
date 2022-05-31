3e9167cfd87c20628a891d9e837d894dc0bef5a7
Amos <volund@gmail.com>
2022-05-31 06:13:41 +0200

    openssln ios

--- build-workspace/scripts/openssl-1.1.1n/build/ios/universal-debug/run-direct_device.sh
+++ build-workspace/scripts/openssl-1.1.1n/build/ios/universal-debug/run-direct_device.sh
@@ -13,7 +13,7 @@ IOS_TARGET_VERSION=10.0
 IOS_SDK=iPhoneOS${IOS_SDK_VERSION}.sdk
 IOS_SDKS_FOLDER=${XCODE_ROOT}/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/
 
-CROSS_TOP="${IOS_SDKS_FOLDER}" CROSS_SDK="${IOS_SDK}" CROSS_COMPILE="${XCRUN_FOLDER}" CFLAGS="-arch armv7 -arch armv7s -arch arm64" ${RELATIVE_SOURCE}/Configure ios-xcrun no-shared --prefix=${ROOT}/${BUILD_FOLDER} --openssldir=${ROOT}/${BUILD_FOLDER}
+CROSS_TOP="${IOS_SDKS_FOLDER}" CROSS_SDK="${IOS_SDK}" CROSS_COMPILE="${XCRUN_FOLDER}" CFLAGS="-arch armv7 -arch armv7s -arch arm64" ${RELATIVE_SOURCE}/Configure ios-xcrun no-shared no-dso no-hw no-engine --prefix=${ROOT}/${BUILD_FOLDER} --openssldir=${ROOT}/${BUILD_FOLDER}
 make clean
 make
 make install
