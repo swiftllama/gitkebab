8ecc2ee974da60c5d1bcf798ce8a8765598e51b6
Amos <volund@gmail.com>
2022-06-01 04:59:11 +0200

    pcre ios

--- build-workspace/scripts/pcre-8.45/build/ios/universal-debug/run-direct_universal.sh
+++ build-workspace/scripts/pcre-8.45/build/ios/universal-debug/run-direct_universal.sh
@@ -6,7 +6,7 @@ define_basic_variables "pcre-8.45" "ios" "universal" "debug"
 init_and_change_into_tmp_build_folder
 
 rm -rf ${ROOT}/${BUILD_FOLDER}
-cp -PR ${ROOT}/${BUILD_FOLDER} ${ROOT}/${BUILD_FOLDER}_device
+cp -PR ${ROOT}/${BUILD_FOLDER}_device ${ROOT}/${BUILD_FOLDER}
 
 lipo -create ${ROOT}/${BUILD_FOLDER}_device/lib/libpcre.a ${ROOT}/${BUILD_FOLDER}_simulator/lib/libpcre.a -output ${ROOT}/${BUILD_FOLDER}/lib/libpcre.a
 lipo -create ${ROOT}/${BUILD_FOLDER}_device/lib/libpcreposix.a ${ROOT}/${BUILD_FOLDER}_simulator/lib/libpcreposix.a -output ${ROOT}/${BUILD_FOLDER}/lib/libpcreposix.a
