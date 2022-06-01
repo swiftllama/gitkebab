1e9c499764e6046b2d18ad5b3ec55e0efad9ba90
Amos <volund@gmail.com>
2022-06-01 10:43:23 +0200

    libssh2 ios

--- build-workspace/scripts/libssh2-1.10.0/source/clone.sh
+++ build-workspace/scripts/libssh2-1.10.0/source/clone.sh
@@ -9,6 +9,7 @@ git clone --branch libssh2-1.10.0 --depth 1 git@github.com:libssh2/libssh2.git s
 echo "Applying patch"
 cd source/libssh2-1.10.0
 git apply ../../patches/libssh2-1.10.0/retry-on-eintr.diff
+git apply ../../patches/libssh2-1.10.0/include-errno-on-ios.diff
 
 set +x
 echo "--- DONE ---"
