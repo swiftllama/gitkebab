f3758a7c40d3864faf946033297bd9a2da44570c
Amos <volund@gmail.com>
2022-06-01 14:33:55 +0200

    gitkebab ios

--- build-workspace/scripts/gitkebab-head/build/ios/universal-debug/run.sh
+++ build-workspace/scripts/gitkebab-head/build/ios/universal-debug/run.sh
@@ -8,8 +8,8 @@ define_basic_docker_variables
 echo "--- Building directly on the mac ---"
 
 ./${BUILD_SCRIPTS}/run-direct_device.sh
-./${BUILD_SCRIPTS}/run-direct_simulator.sh
-./${BUILD_SCRIPTS}/run-direct_universal.sh
+#./${BUILD_SCRIPTS}/run-direct_simulator.sh
+#./${BUILD_SCRIPTS}/run-direct_universal.sh
 
 #echo "--- Exiting docker build environment ---"
 #${BUILD_SCRIPTS}/run-direct-2_generate-dart-bindings.sh
