db8b3af8a003690df79a9df042a63c171cc7cb63
Amos <volund@gmail.com>
2022-06-01 14:43:12 +0200

    zlib

--- build-workspace/scripts/zlib-1.2.12/build/ios/universal-debug/run-direct_device.sh
+++ build-workspace/scripts/zlib-1.2.12/build/ios/universal-debug/run-direct_device.sh
@@ -5,15 +5,19 @@ set -x
 define_basic_variables "zlib-1.2.12" "ios" "universal" "debug"
 init_and_change_into_tmp_build_folder
 
+rm -rf ${ROOT}/${BUILD_FOLDER}_device
+
 XCODE_ROOT=/Applications/Xcode.app
 IOS_SDK_VERSION=15.5
 IOS_TARGET_VERSION=10.0
 IOS_SDK=${XCODE_ROOT}/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS${IOS_SDK_VERSION}.sdk
 
 
-CFLAGS="-O3 -arch armv7 -arch armv7s -arch arm64 -isysroot $IOS_SDK -mios-version-min=${IOS_TARGET_VERSION} -fembed-bitcode" ${RELATIVE_SOURCE}/configure --prefix=${ROOT}/${BUILD_FOLDER}_device
+CFLAGS="-O3 -arch armv7 -arch armv7s -arch arm64 -isysroot $IOS_SDK -mios-version-min=${IOS_TARGET_VERSION} -fembed-bitcode" ${RELATIVE_SOURCE}/configure --prefix=${ROOT}/${BUILD_FOLDER}
 make clean
 make
 make install
+
+mv ${ROOT}/${BUILD_FOLDER}_device ${ROOT}/${BUILD_FOLDER}
 
 print_done
--- build-workspace/scripts/zlib-1.2.12/build/ios/universal-debug/run-direct_universal.sh
+++ build-workspace/scripts/zlib-1.2.12/build/ios/universal-debug/run-direct_universal.sh
@@ -5,7 +5,8 @@ set -x
 define_basic_variables "zlib-1.2.12" "ios" "universal" "debug"
 init_and_change_into_tmp_build_folder
 
-mkdir -p ${ROOT}/${BUILD_FOLDER}/lib
+rm -rf ${ROOT}/${BUILD_FOLDER}
+cp _PR ${ROOT}/${BUILD_FOLDER}_device ${ROOT}/${BUILD_FOLDER}
 lipo -create ${ROOT}/${BUILD_FOLDER}_device/lib/libz.a ${ROOT}/${BUILD_FOLDER}_simulator/lib/libz.a -output ${ROOT}/${BUILD_FOLDER}/lib/libz.a
 
 print_done
