06d77c93674dd23215d8fe3fa0622d8f3002f273
Amos Joshua <buildmachine2@buildmachine2-machub.local>
2022-06-08 17:20:27 +0200

    frameworks macos gitkebab build

--- build-workspace/scripts/libiconv-1.17/build/macos/universal-debug/run-direct.sh
+++ build-workspace/scripts/libiconv-1.17/build/macos/universal-debug/run-direct.sh
@@ -10,7 +10,7 @@ init_and_change_into_tmp_build_folder
 ##    - CFLAGS=-fPIC
 ##
 
-FLAGS="-fPIC --target=x86_64-apple-macos -arch x86_64 -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX12.3.sdk -mmacosx-version-min=10.11"
+FLAGS="-fPIC --target=x86_64-apple-macos -arch x86_64 -arch arm64  -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX12.3.sdk -mmacosx-version-min=10.11"
 ${RELATIVE_SOURCE}/configure --enable-static --prefix=${ROOT}/${BUILD_FOLDER} CFLAGS="${FLAGS}" CXXFLAGS="${FLAGS}" LDFLAGS="${FLAGS}"
 make
 make install
